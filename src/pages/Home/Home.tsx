fc1a7697456835a428a19403f83a7b1f92e6fdad
Bukhtyar Haider Khan <bukhtyar.haider1@gmail.com>
2024-04-18 21:31:03 +0500

    remove: unnecessary comments and imports for Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,25 @@
 import FAQSection from "../../components/FAQSection/FAQSection";
 import TruckJourney from "../../components/TruckJourney/TruckJourney";
 import Footer from "../../components/Footer/Footer";
-import OurServiceHeader from "../../components/OurServiceHeader/OurServiceHeader";
-import ComprehensiveSolutions from "../../components/ComprehensiveSolutions/ComprehensiveSolutions";
-import WhatWeOffer from "../../components/WhatWeOffer/WhatWeOffer";
-import OurSpecializedFleet from "../../components/OurSpecializedFleet/OurSpecializedFleet";
+import HeroSection from "../../components/HeroSection/HeroSection";
+import Collaborations from "../../components/Collaborations/Collaborations";
+import EmpoweringInsights from "../../components/EmpoweringInsights/EmpoweringInsights";
+import JoinUs from "../../components/JoinUs/JoinUs";
+import OurServices from "../../components/OurServices/OurServices";
+import ReviewsSection from "../../components/ReviewsSection/ReviewsSection";
+import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 
 
 const Home = () => {
     return (
         <div>
-            {/* <HeroSection />
+            <HeroSection />
             <Collaborations />
             <OurServices />
             <JoinUs />
             <EmpoweringInsights />
             <ReviewsSection />
             <WhyChooseUs />
-            <FAQSection />
-            <TruckJourney /> */}
-            <OurServiceHeader />
-            <ComprehensiveSolutions />
-            <WhatWeOffer />
-            <OurSpecializedFleet />
             <FAQSection />
             <TruckJourney />
             <Footer />
