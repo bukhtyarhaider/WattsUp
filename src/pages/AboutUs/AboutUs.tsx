4422e11df86db0491229083628522622261a6864
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-04-19 02:10:02 +0500

    [APR24-WU-3] - Milestone 3

---
+++ src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import LazyLoad from 'react-lazy-load';
+import styles from './AboutUs.module.scss';
+import { aboutUsImg } from '../../assets';
+import { aboutUsData } from '../../content';
+
+const AboutUs = () => {
+    return (
+        <div className={styles.aboutUsContainer}>
+            <div className={styles.imgContainer}>
+                <LazyLoad>
+                    <img src={aboutUsImg} alt="About Us" />
+                </LazyLoad>
+                <div className={styles.imgContent}>
+                    <h2 className={styles.heading}>About Us - Our Comprehensive Solutions</h2>
+                    <p className={styles.description}>From efficient load finding to meticulous paperwork exchange, we have everything you need to streamline your logistics process and keep your business moving forward.</p>
+                </div>
+            </div>
+
+            <div className={styles.contentContainer}>
+                {
+                    aboutUsData.map((data, index) => (
+                        <React.Fragment key={index}>
+                            <h2 className={styles.contentHeading}>{data.heading}</h2>
+                            <p className={styles.contentDescription}>{data.description}</p>
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
